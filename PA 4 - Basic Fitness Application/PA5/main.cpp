1272886df35cb50ea850a8fa46c506ea5340af5f
Mark 篠崎 <44478527+MarkShinozaki@users.noreply.github.com>
2024-06-15 19:50:12 -0700

    Add files via upload

---
+++ PA 4 - Basic Fitness Application/PA5/main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+* Programmer: Mark Shinozaki											      *
+* Class: CptS 122; Lab Section 01                                             *
+* Programming Assignment: PA4 Basic Fitness Application                       *
+* Date:  10/13/2021                                                           *
+*                                                                             *
+* Description: This program is a basic fitness application                    *
+******************************************************************************/
+
+#include "Fitness.h"
+
+int main(void)
+{
+	FitnessAppWrapper app;
+
+	app.runApp();
+
+
+	return 0;
+}
